36638a8cb7992a16b8b5f35c3ea8051a55130eaa
Jethro Kuan <jethrokuan95@gmail.com>
2013-06-08 20:29:38 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YOLo::Application.config.secret_key_base = 'edb395e3c6b6084e83767a15098643f778804a446d1316dc0775a5d2f06b763d548c78f0803ed814431861f9f4768097dddb8c9e0cb0d677219c5c8f94c6e4ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YOLo::Application.config.session_store :cookie_store, key: '_yOLo_session'
